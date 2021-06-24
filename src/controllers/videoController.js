414d9ca8ca7ff4f8237587068f5b8ef17ce11a05
trazomssome <rkdtmdwn@gmail.com>
2021-06-24 11:56:34 +0900

    make comment realtime-like
    코멘트 섹션을 리얼타임처럼 보이게 변경. 리얼타임"처럼 보이게"인 이유는 실제로 데이터베이스를 불러와서 보이게 하는 것이 아닌 js를 통해 html을 추가하여 임시로 댓글을 보여주는 방식이기 때문

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,6 @@
 import Video from "../models/Video";
 import User from "../models/User";
+import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
   const videos = await Video.find({})
@@ -10,7 +11,7 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
   if (video === null) {
     return res.render("404", { pageTitle: "Video not found." });
   }
@@ -120,4 +121,24 @@ export const registerView = async (req, res) => {
   video.meta.views = video.meta.views + 1;
   await video.save();
   return res.sendStatus(200);
+};
+
+export const createComment = async (req, res) => {
+  const {
+    session: { user },
+    body: { text },
+    params: { id },
+  } = req;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.sendStatus(404);
+  }
+  const comment = await Comment.create({
+    text,
+    owner: user._id,
+    video: id,
+  });
+  video.comments.push(comment._id);
+  video.save();
+  return res.status(201).json({ newCommentId: comment._id });
 };
--- todo.md
+++ todo.md
@@ -2,7 +2,7 @@
 
 ## 버그
 
-1. Github 기본아바타 제대로 나오게하기
+1. Github 기본아바타 http://localhost:4000/https://avatars.githubusercontent.com/u/49433915?v=4 404 (Not Found)
 2. 녹화 영상 자동으로 업로드하기(아니면 녹화 분리하기)
 3. 업로드시 file too large 에러 컨트롤하기
 4. 적재적소에 flash message 삽입하기
@@ -14,9 +14,16 @@
 1. 검색창 가운데로 옮기고 항상 보여주기(유튜브스럽게)
 1. 기본아바타 추가(github 가입시 기본아바타 사용하기)
 1. 존재하지 않는 링크에 접근할 시 띄울 페이지(404페이지)
+1. mixin을 통해 댓글 개선하기
 
 ## 기능
 
+1. 댓글삭제
+   - fetch request(method:"Delete")
+   - api router 추가
+   - controller
+   - 소유자에게만 삭제버튼 노출
+   - html을 삭제시켜서 실시간으로 댓글 삭제시키기
 1. 업로드 영상 크기 줄여 업데이트하게하기
 1. 업로드 프로필 크기 줄여 업데이트하게하기
 1. Thumbnail 자동으로 추출하게 하기 or 기본썸네일 만들기
@@ -26,6 +33,7 @@
    - 구글 : https://console.cloud.google.com/projectselector2/apis/dashboard?pli=1&supportedpurview=project
 1. video play 단축키 추가(볼륨조절 등등)
 1. Recording 카운트다운 표시
+1. 유튜브 링크만으로 영상을 올릴 수 있도록
 
 ## 불편함
 
