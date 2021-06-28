a411aace2846714647e59b1da930a2bbf9cde67c
trazomssome <rkdtmdwn@gmail.com>
2021-06-28 23:03:27 +0900

    input file 용량제한 bugfix

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -8,7 +8,7 @@ const inputFile = document.querySelector(".upload__video__file");
 const handleChangeFile = (event) => {
   if (inputFile.files[0] && inputFile.files[0].size > 10 * MB - 1) {
     alert("10MB 이하의 비디오만 업로드 가능합니다.");
-    inputFile = inputFile;
+    inputFile.value = "";
   }
   return;
   //파일 상태를 null로 만들 수 없다면 upload 버튼을 막아야 하는가?
