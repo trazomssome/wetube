6e1386b0e31f42350781db676e6d053c51d79991
trazomssome <rkdtmdwn@gmail.com>
2021-06-28 16:18:28 +0900

    local환경에서 버그 수정

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,7 +6,7 @@ export const home = async (req, res) => {
   const videos = await Video.find({})
     .sort({ createdAt: "desc" })
     .populate("owner");
-  return res.render("home", { pageTitle: "Youtube-Clone", videos });
+  return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const watch = async (req, res) => {
